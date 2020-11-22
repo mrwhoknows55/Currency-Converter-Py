e3e4d12bad34029f10ddd6a2724854a78db3ffe8
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-11-22 21:56:37 +0530

    Optimize UI

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import json_to_csv
 SMALL_FONT = ("Fira Sans", 12)
 MID_FONT = ("Fira Sans", 19)
 LARGE_FONT = ("Fira Sans", 32)
+FONT_NORMAL = ("Roboto", 12)
 
 
 class CurrencyApp(ThemedTk):
@@ -22,11 +23,11 @@ class CurrencyApp(ThemedTk):
         # __init__ function for class Tk
         ThemedTk.__init__(self, *args, **kwargs)
         self.set_theme("breeze")
-        self.minsize(1500, 900)
+        self.minsize(1050, 720)
 
         # creating a container
         container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
+        container.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         # initializing frames to an empty array
         self.frames = {}
@@ -59,6 +60,7 @@ class HomePage(tk.Frame):
 
         base = self.currency_from.get()[0:3]
         sec = self.currency_to.get()[0:3]
+
         try:
             amount = float(self.entry_amount.get())
         except ValueError:
@@ -121,24 +123,24 @@ class HomePage(tk.Frame):
         label = ttk.Label(self, text="Currency Converter", font=MID_FONT)
         label.grid(row=0, column=1, pady=20)
 
-        label_from = ttk.Label(self, text="From:", padding=10, font="Helvetica 14 normal")
+        label_from = ttk.Label(self, text="From: ", padding=10, font=FONT_NORMAL)
         menu_from = ttk.OptionMenu(self, self.currency_from, *self.currencyList)
         self.currency_from.set(self.currencyList[15])
         label_from.grid(row=1, column=0, sticky=tk.E, padx=40, pady=5)
         menu_from.grid(row=1, column=1, stick=tk.W, ipady=3, ipadx=7)
 
-        label_amount_1 = ttk.Label(self, text="Amount", padding=10, font="Helvetica 14 normal")
-        entry_amount = ttk.Entry(self, textvariable=self.entry_amount, font="Helvetica 14 normal")
+        label_amount_1 = ttk.Label(self, text="Amount: ", padding=10, font=FONT_NORMAL)
+        entry_amount = ttk.Entry(self, textvariable=self.entry_amount, font=FONT_NORMAL)
         label_amount_1.grid(sticky=tk.E, padx=40, pady=5)
         entry_amount.grid(row=2, column=1, stick=tk.W, ipady=3, ipadx=7)
 
-        label_to = ttk.Label(self, text="To:", padding=10, font=("bold", 12,))
+        label_to = ttk.Label(self, text="To: ", padding=10, font=FONT_NORMAL)
         menu_to = ttk.OptionMenu(self, self.currency_to, *self.currencyList)
         self.currency_to.set(self.currencyList[31])
         label_to.grid(sticky=tk.E, padx=40, pady=5)
         menu_to.grid(row=3, column=1, sticky=tk.W, ipady=3, ipadx=7)
 
-        label_amount_2 = ttk.Label(self, text="Amount", padding=10, font="Helvetica 14 normal")
+        label_amount_2 = ttk.Label(self, text="Amount: ", padding=10, font=FONT_NORMAL)
         label_amount_2.grid(row=4, column=0, sticky=tk.E, padx=40, pady=5)
 
         label_final_amount = ttk.Entry(self, textvariable=self.final_amount, font=("bold", 12,))
@@ -154,24 +156,20 @@ class HomePage(tk.Frame):
 
 class History(tk.Frame):
 
-    # TODO: graph shouldn't be stacked
-    def show_graph(self):
+    def show_graph(self, base_sym, to_sym):
         # analyze data and plt
         sampleData = pandas.read_csv('historical_data.csv')
-        fig = Figure(figsize=(15, 6), dpi=100)
+        fig = Figure(figsize=(10, 5), dpi=100)
         plt = fig.add_subplot(111)
 
         list_name = self.pack_slaves()
         c = 1
         for item in list_name:
-            if c == 8:
+            if c == 4:
                 plt.clear()
                 item.destroy()
             c += 1
 
-        # TODO: add it to fun params
-        base_sym = self.currency_base.get()[0:3]  # TODO: change to vars
-        to_sym = self.currency_to.get()[0:3]
         title = "1 " + base_sym + " VS " + to_sym
         plt.set_title(title)
         plt.plot(sampleData.date, sampleData.get(to_sym), '-', label=to_sym)
@@ -181,8 +179,7 @@ class History(tk.Frame):
 
         canvas = FigureCanvasTkAgg(fig, self)
         canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-        # toolbar = NavigationToolbar2Tk(canvas, self)
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True, padx=20, pady=20)
 
     def get_history_data(self):
         span = str(self.history_span.get())
@@ -195,9 +192,12 @@ class History(tk.Frame):
         else:
             days_before = (date.today() - timedelta(days=30)).isoformat()
 
-        # TODO: use variable from fun param
-        json_to_csv.main(self.currency_base.get()[0:3], self.currency_to.get()[0:3], days_before, current_date)
-        self.show_graph()
+        # get selected currencies from dropdown menu
+        base_sym = self.currency_base.get()[0:3]
+        to_sym = self.currency_to.get()[0:3]
+
+        json_to_csv.main(base_sym, to_sym, days_before, current_date)
+        self.show_graph(base_sym, to_sym)
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -246,26 +246,29 @@ class History(tk.Frame):
         self.history_span = tk.StringVar()
         self.date_span_list = ['', 'Week', 'Half Month', 'Month']
 
-        menu_base_currencies = ttk.OptionMenu(self, self.currency_base, *self.currencyList)
+        menu_frame = tk.Frame(self)
+        menu_frame.pack(side=tk.TOP)
+
+        menu_base_currencies = ttk.OptionMenu(menu_frame, self.currency_base, *self.currencyList)
         self.currency_base.set(self.currencyList[31])
-        menu_base_currencies.pack(ipady=3, ipadx=7, pady=5)
+        menu_base_currencies.pack(side=tk.LEFT, padx=10, ipady=3, ipadx=7)
 
-        label_vs = ttk.Label(self, text="VS")
-        label_vs.pack(after=menu_base_currencies)
+        label_vs = ttk.Label(menu_frame, text="VS")
+        label_vs.pack(side=tk.LEFT, padx=5)
 
-        menu_to_currencies = ttk.OptionMenu(self, self.currency_to, *self.currencyList)
+        menu_to_currencies = ttk.OptionMenu(menu_frame, self.currency_to, *self.currencyList)
         self.currency_to.set(self.currencyList[15])
-        menu_to_currencies.pack(pady=5, ipady=3, ipadx=7)
+        menu_to_currencies.pack(side=tk.LEFT, padx=5, ipady=3, ipadx=7)
 
-        menu_history_span = ttk.OptionMenu(self, self.history_span, *self.date_span_list)
+        menu_history_span = ttk.OptionMenu(menu_frame, self.history_span, *self.date_span_list)
         self.history_span.set(self.date_span_list[1])
-        menu_history_span.pack(pady=5, ipady=3, ipadx=7)
+        menu_history_span.pack(side=tk.LEFT, fill=tk.X, padx=5, ipady=3, ipadx=7)
 
-        button_get_history = ttk.Button(self, text="Get History", command=self.get_history_data)
-        button_get_history.pack(pady=5, ipady=3, ipadx=7)
+        button_get_history = ttk.Button(menu_frame, text="Get History", command=self.get_history_data)
+        button_get_history.pack(side=tk.LEFT, fill=tk.X, pady=5, ipady=3, ipadx=7)
 
         button_goto_home = ttk.Button(self, text="Home", command=lambda: controller.show_frame(HomePage))
-        button_goto_home.pack(pady=5, ipady=3, ipadx=7)
+        button_goto_home.pack(ipady=3, ipadx=7, side=tk.BOTTOM)
 
 
 # Driver Code
